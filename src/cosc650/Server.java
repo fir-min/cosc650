109937451b90048e7657117488adc38cf23bf85f
Firmin Saint-Amour <firminsa@gmail.com>
2019-05-04 10:07:04 -0400

    updating server

--- src/cosc650/Server.java
+++ src/cosc650/Server.java
@@ -1,50 +1,86 @@
 package cosc650;
 
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.net.SocketException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.util.Arrays;
+import java.util.Scanner;
 
-public class Server extends Thread {
+public class Server{
+    static boolean sendPackets = true;
+    static int packetsSent = 0;
+    static byte[] dataToSend = null;
+    static int udpPort = 0;
+    static File testFile = null;
+    static String path = Server.class.getResource("stestfile.txt").getPath();
+    static int timeout = 0;
+    static int packetSize = 0;
+    static Scanner scanner = new Scanner(System.in);
+    static DatagramSocket udpSocket;
+    static byte[] buf = new byte[256];
 
-    String path = Server.class.getResource("stestfile.txt").getPath();
+    public static void start() {
+        try {
+            testFile = new File(path);
+            System.out.print("Enter an integer timeout T in ms (e.g., 100 means 100 ms): ");
+            timeout = scanner.nextInt();
+            System.out.print("Enter the size M of all packets (except the last) in bytes (e.g., 1200 means 1200 bytes): ");
+            packetSize = scanner.nextInt();
 
-    private DatagramSocket socket;
-    private byte[] buf = new byte[256];
+            dataToSend = Files.readAllBytes(testFile.toPath());
 
-    public Server() throws SocketException {
-        socket = new DatagramSocket(4445);
-    }
-
-    public void run() {
-        boolean running = true;
-
-        while (running) {
-            DatagramPacket packet
-                    = new DatagramPacket(buf, buf.length);
-            try {
-                socket.receive(packet);
-            } catch (IOException e) {
-                e.printStackTrace();
+            if (!(packetSize <= testFile.length())) {
+                System.out.println("Packet size should be less than or equal to the file size");
+                System.exit(404);
             }
 
-            InetAddress address = packet.getAddress();
-            int port = packet.getPort();
-            packet = new DatagramPacket(buf, buf.length, address, port);
-            String received
-                    = new String(packet.getData(), 0, packet.getLength());
+            // if all is well start tcp connection
 
-            if (received.equals("end")) {
-                running = false;
-                continue;
+            ServerSocket tcpSocket = null;
+
+            tcpSocket = new ServerSocket(21111);
+            Socket clientSocket = tcpSocket.accept();
+            PrintWriter output = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            String hello = input.readLine();
+            if (!"hello".contains(hello)) {
+                System.exit(4);
             }
-            try {
-                socket.send(packet);
-            } catch (IOException e) {
-                e.printStackTrace();
+            // the message contains hello
+            output.println("Hello Received - UDP port: "  + hello.split(" ")[1]);
+            udpPort = Integer.parseInt(hello.split(" ")[1]);
+
+            // now start udp connection
+            udpSocket = new DatagramSocket(udpPort);
+            while (sendPackets) {
+                byte[] toSend = null;
+                if (packetsSent == 0) {
+                    toSend = Arrays.copyOfRange(dataToSend, packetsSent, packetSize);
+                } else {
+                    toSend = Arrays.copyOfRange(dataToSend, packetsSent, packetSize);
+                }
+
+                DatagramPacket dp = new DatagramPacket(toSend, toSend.length, InetAddress.getLocalHost(), udpPort);
+
+                packetsSent++;
             }
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        socket.close();
     }
+
+    public static void main(String[] args) {
+        start();
+    }
+
+
 }
